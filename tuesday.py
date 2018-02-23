a144cdc38e6cbcb0cc2936dcf9c834f4abac7f31
G00364690 <35767314+G00364690@users.noreply.github.com>
2018-02-23 16:58:50 +0000

    Create tuesday.py

---
+++ tuesday.py
@@ -0,0 +1,9 @@
+# Declan Reidy - is it Tuesday - 23-02-2018
+
+import datetime
+
+if datetime.datetime.today().weekday() == 1:
+
+    print("Yay, it's Tuesday!")
+else:
+    print("Unfortunately it's not Tuesday!")
