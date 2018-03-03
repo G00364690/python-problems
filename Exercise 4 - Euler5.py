9c2edabbf6deeb05bc079ac9726e74f912f5995e
G00364690 <35767314+G00364690@users.noreply.github.com>
2018-03-03 15:55:45 +0000

    Update Exercise 4 - Euler5.py

--- Exercise 4 - Euler5.py
+++ Exercise 4 - Euler5.py
@@ -9,7 +9,7 @@
 from math import factorial
 i = 2520
 
-for i in range(i,(factorial(20)),20):
+for i in range(i,factorial(20),20):
     if (i % 20 ==0) and (i % 19 ==0) and (i % 18 ==0) and (i % 17 ==0) and (i % 16 ==0) and (i % 15 ==0) and (i % 14 ==0) and (i % 13 ==0) and (i % 12 ==0) and (i % 11 ==0):
       print("The smallest Euler number is:",i)
       break
