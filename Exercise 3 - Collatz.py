a654666ef26b999bc58c0bf9e5331ee7b339781a
G00364690 <35767314+G00364690@users.noreply.github.com>
2018-04-08 23:58:18 +0100

    Update Exercise 3 - Collatz.py

--- Exercise 3 - Collatz.py
+++ Exercise 3 - Collatz.py
@@ -3,15 +3,15 @@
 # Collatz conjecture program - https://en.wikipedia.org/wiki/Collatz_conjecture
 
 n = int(input("Please enter an integer: "))
-x = n
+x = n #assign n to x so we can call it later without confusion
 
-while n >= 2:
+while n >= 2: #so long as n is not less than 2
 
-  if n % 2 == 0:
-    n = n/2
+  if n % 2 == 0: #test if n mod 2 is equal to 0
+    n = n/2 #if true divide n by 2
     print(int(n))
   else:
-    n = (n*3)+1
+    n = (n*3)+1 #if not true multiply by 3 and add 1.
     print(int(n))
   
-print("Declan has proven the Collatz conjecture holds for the number", x)
+print("Declan has proven the Collatz conjecture holds for the number", x) #x called
