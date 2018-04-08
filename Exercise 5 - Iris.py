795aa85820c3873329ce512ab29c2ec9f3835dc5
G00364690 <35767314+G00364690@users.noreply.github.com>
2018-04-08 23:47:40 +0100

    Update Exercise 5 - Iris.py

--- Exercise 5 - Iris.py
+++ Exercise 5 - Iris.py
@@ -8,7 +8,12 @@
 # length and sepal width, and these values should have the decimal places 
 # aligned, with a space between the columns.
 
-with open("data/iris.csv") as f:
-  for line in f:
+# Line.split command adapted from openfile.py tutorial by Ian McLoughlin.
+
+with open("data/iris.csv") as f: #Read data file iris.csv
+  for line in f: #for each line in the file
     
-   print(line.split(",")[0],"",line.split(",")[1],"",line.split(",")[2],"",line.split(",")[3])
+    print(line.split(",")[0],"",line.split(",")[1],"",line.split(",")[2],"",line.split(",")[3])
+
+   #Line 14: Delimit the data in the file by comma and add a space between each.
+   #Line 14: separate data into positions 0-3 (1-4)
