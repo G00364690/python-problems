632cddac5efa5f5df7b668a2cd942d034640ed84
G00364690 <35767314+G00364690@users.noreply.github.com>
2018-04-08 23:32:48 +0100

    Update Exercise 4 - Euler5.py

--- Exercise 4 - Euler5.py
+++ Exercise 4 - Euler5.py
@@ -1,17 +1,22 @@
 # Declan Reidy - 23-02-2018
 #
-# Updated with factorial function 03-03-2018
+# Updated with import math factorial function 03-03-2018
 #
-# Updated with factorial function 08-04-2018
+# Updated with defined factorial function 08-04-2018. Function defined for exercise 6 is more appropriate than using the import math function
 #
 # Euler problem 5 - Exercise 4
 #
 # 2520 is the smallest number divisible by all numbers 1 to 10
 
-from math import factorial
-i = 2520
+def factorial(upto): #defne factorial function
+    product = 1
+    for i in range(1, upto + 1): #perform operation for i values 1-20
+      product = product * i #new product is given the value old product multiplied by i
+    return (product)
 
-for i in range(i,factorial(20),20):
+i = 2520 #no need to start lower than 2520
+
+for i in range(i,factorial(20),20): #every number divisible by 1-20 has to be divisible by 20. increments of 20 will find the solution quicker)
     if (i % 20 ==0) and (i % 19 ==0) and (i % 18 ==0) and (i % 17 ==0) and (i % 16 ==0) and (i % 15 ==0) and (i % 14 ==0) and (i % 13 ==0) and (i % 12 ==0) and (i % 11 ==0):
-      print("The smallest Euler number is:",i)
+      print("The smallest number divisible by 1-20 is:",i)
       break
