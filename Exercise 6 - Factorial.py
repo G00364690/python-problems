3aa941770aafb267ecfbbdaf3bb159bc7da60b31
G00364690 <35767314+G00364690@users.noreply.github.com>
2018-04-08 23:54:46 +0100

    Update Exercise 6 - Factorial.py

--- Exercise 6 - Factorial.py
+++ Exercise 6 - Factorial.py
@@ -8,11 +8,13 @@
 # 5x4x3x2x1 which equals 120. You should, in your script, test the 
 # function by calling it with the values 5, 7, and 10.
 
+# Source - adapted from sumall() from function.py Ian McLoughlin
+
 
 def factorial(upto):
     product = 1
-    for i in range(1, upto + 1):
-      product = product * i
+    for i in range(1, upto + 1): #perform operation for i values 1-20
+      product = product * i #new product is given the value old product multiplied by i
     return (product)
 print("The factorial of the input is",factorial(5))
 print("The factorial of the input is",factorial(7))
